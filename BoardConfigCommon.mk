LineageOS/android_device_xiaomi_msm8953-common

msm8953-common: Move vendor sepolicy to sepolicy/vendor
Change-Id: Ib08dbaa30c9f603ec2e36590e4a4ae47cd81439a

BoardConfigCommon.mk
@@ -162,7 +162,7 @@ endif
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true

raidenkkj/android_device_xiaomi_mithorium-common

BoardConfigCommon.mk
@@ -163,7 +163,7 @@ endif
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true